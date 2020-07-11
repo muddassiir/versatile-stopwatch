72a8f8523ba035a8f570ca959b5adf8a4c387a21
muddassiir <muhammadmuddassir777@gmail.com>
2020-07-11 15:22:49 -0700

    deploy message

--- stop watch/app.js
+++ stop watch/app.js
@@ -4,30 +4,34 @@ var min=document.getElementById("min");
 var milisec=0;
 var second=0;
 var minutes=0;
-var timer;
+var interval;
 
 function timer(){
     milisec++
     msec.innerHTML=milisec;
     if(milisec >= 100){
         second++
-        sec.innerHTML=second
-        milisec=0
-    }else if(second >= 5){
+        sec.innerHTML=second;
+        milisec=0;
+    }else if(second >= 60){
         minutes++
-        min.innerHTML=minutes
-        second=0
+        min.innerHTML=minutes;
+        second=0;
     }
 }
-
+var btn=document.getElementById('start');
+var startbtn=btn.querySelector('button')
 function start(){
-    timer=setInterval(timer,10);
+    interval=setInterval(timer,10)
+    document.getElementById("Button").disabled = true;
 }
 function stop(){
-    clearInterval(timer);
+    clearInterval(interval)
+    document.getElementById("Button").disabled = false;
 }
 function reset(){
     msec.innerHTML=0;
     sec.innerHTML=0;
     min.innerHTML=0;
+    stop()
 }
